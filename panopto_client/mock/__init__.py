7a4496dd3907f7b89bbac30ae3e9b1874803ac14
Bradley Bell <bradleyb@uw.edu>
2016-11-01 14:12:45 -0700

    Support Django >= 1.9
    django.utils.log.getLogger does not appear to have been part of the public API, and no longer works.
    django.utils.importlib was deprecated and has been removed.

--- panopto_client/mock/__init__.py
+++ panopto_client/mock/__init__.py
@@ -2,8 +2,13 @@
 Panopto API mock data class
 """
 from django.conf import settings
-from django.utils.importlib import import_module
-from django.utils.log import getLogger
+
+try:
+    from importlib import import_module
+except ImportError:
+    from django.utils.importlib import import_module
+
+from logging import getLogger
 from hashlib import md5
 import sys
 import os
